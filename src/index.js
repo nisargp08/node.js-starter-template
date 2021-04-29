d44ce61bba11024947c843b127381a9aac1856a4
Nisarg Patel <nisargp42@gmail.com>
2021-04-28 21:46:17 -0400

    Compression package added

--- src/index.js
+++ src/index.js
@@ -2,12 +2,16 @@ import express from 'express';
 import { json, urlencoded } from 'body-parser';
 import morgan from 'morgan';
 import cors from 'cors';
+import compression from 'compression';
 
 // Initialize express app
 const app = express();
 
 // Middlewares
 app.use(cors());
+app.use(compression({
+  level : 6,
+}))
 app.use(json());
 app.use(urlencoded({ extended: true }));
 app.use(morgan('dev'));
